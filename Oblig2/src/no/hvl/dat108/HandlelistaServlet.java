dc3935e4ae22c8d9c948f05279d864ce6a12da5f
S571736 <Sondre.Gjesdal@gmail.com>
2018-10-05 14:07:20 +0200

    endringer i login og handlelista

--- Oblig2/src/no/hvl/dat108/HandlelistaServlet.java
+++ Oblig2/src/no/hvl/dat108/HandlelistaServlet.java
@@ -27,36 +27,37 @@ public class HandlelistaServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		ArrayList<Vare> liste = new ArrayList<>();
-		HttpSession sesjon = request.getSession(true);
+		HttpSession sesjon = request.getSession(false);
 
-		if (sesjon.getAttribute("liste") == null) {
-			sesjon.setAttribute("liste", new HandleVogn());
+		if (sesjon == null) {
+			response.sendRedirect("loginhandler?timeOut");
+		} else {
+			HandleVogn vogn = (HandleVogn) sesjon.getAttribute("liste");
+
+			response.setContentType("text/html; charset=ISO-8859-1");
+
+			PrintWriter out = response.getWriter();
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<meta charset=\"ISO-8859-1\">");
+			out.println("<title>Handlelista</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<fieldset><form method=\"post\" action=\"handlelista\">");
+			out.println("<legend><b>Mi Handlelista</b></legend>");
+			out.println("<p><input type=\"submit\" value=\"Legg til i lista\"/>");
+			out.println("<input type=\"text\" name=\"vare\" placeholder=\"Namn på vare...\"/></p>");
+
+			for (Vare vare : vogn.getVarer()) {
+				out.println(
+						"<p><table><form method=\"post\" action=\"handlelista\"><input type=\"hidden\" name=\"slett\" value=\""
+								+ vare.getNamn() + "\"><input type=\"submit\" name=\"" + vare.getNamn()
+								+ "\" value=\"Slett\"/>" + vare.getNamn() + "</form></table></p>");
+			}
+			out.println("</form></fieldset></body></html>");
+
 		}
-		HandleVogn vogn = (HandleVogn) sesjon.getAttribute("liste");
-
-		response.setContentType("text/html; charset=ISO-8859-1");
-
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<meta charset=\"ISO-8859-1\">");
-		out.println("<title>Handlelista</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<fieldset><form method=\"post\" action=\"handlelista\">");
-		out.println("<legend><b>Mi Handlelista</b></legend>");
-		out.println("<p><input type=\"submit\" value=\"Legg til i lista\"/>");
-		out.println("<input type=\"text\" name=\"vare\" placeholder=\"Namn på vare...\"/></p>");
-
-		for (Vare vare : vogn.getVarer()) {
-			out.println(
-					"<p><table><form method=\"post\" action=\"handlelista\"><input type=\"hidden\" name=\"slett\" value=\""
-							+ vare.getNamn() + "\"><input type=\"submit\" name=\"" + vare.getNamn()
-							+ "\" value=\"Slett\"/>" + vare.getNamn() + "</form></table></p>");
-		}
-		out.println("</form></fieldset></body></html>");
-
 	}
 
 	@Override
@@ -73,14 +74,16 @@ public class HandlelistaServlet extends HttpServlet {
 		String slett = request.getParameter("slett");
 		String vEscaped = escapeHtml(vare);
 		String sEscaped = escapeHtml(slett);
-		
+
 		if (vEscaped != null && vare.length() > 0) {
 			vogn.addVare(vEscaped);
-			System.out.println(vEscaped = Character.toUpperCase(vEscaped.charAt(0)) + vEscaped.substring(1) + " er lagt til i lista!"); //Dette har eg med slik at eg ser at alt blir gjort rett!
+			System.out.println(vEscaped = Character.toUpperCase(vEscaped.charAt(0)) + vEscaped.substring(1)
+					+ " er lagt til i lista!"); // Dette har eg med slik at eg ser at alt blir gjort rett!
 
 		} else if (slett != null && slett.length() > 0) {
 			vogn.removeVare(sEscaped);
-			System.out.println(sEscaped = Character.toUpperCase(sEscaped.charAt(0)) + sEscaped.substring(1) + " er sletta frå lista!"); //Dette har eg med slik at eg ser at alt blir gjort rett!
+			System.out.println(sEscaped = Character.toUpperCase(sEscaped.charAt(0)) + sEscaped.substring(1)
+					+ " er sletta frå lista!"); // Dette har eg med slik at eg ser at alt blir gjort rett!
 
 		}
 
--- Oblig2/src/no/hvl/dat108/loginhandler.java
+++ Oblig2/src/no/hvl/dat108/loginhandler.java
@@ -18,13 +18,15 @@ import static org.apache.commons.lang.StringEscapeUtils.escapeHtml;
 /**
  * Servlet implementation class loginhandler
  */
-@WebServlet(name="loginhandler" , urlPatterns="/loginhandler")
+@WebServlet(name = "loginhandler", urlPatterns = "/loginhandler")
 public class loginhandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		boolean feilPassord = request.getParameter("feilPassord") != null;
+		boolean timeOut = request.getParameter("timeOut") != null;
 		
 		HttpSession sesjon = request.getSession(false);
 		PrintWriter out = response.getWriter();
@@ -47,46 +49,56 @@ public class loginhandler extends HttpServlet {
 		out.println("<body>");
 		out.println("<form action=\"loginhandler\" method=\"post\">");
 		out.println("<fieldset>");
+		if(feilPassord) {
+			out.println("<p>Passordet du skreiv inn var feil. Prøv på nytt:</p>");
+		}
+		else if(timeOut) {
+			out.println("<p>Du har vert inaktiv for lenge... Logg inn på nytt:</p>");
+		}
 		out.println("<legend><b>Skriv inn passordet ditt</b></legend>");
 		out.println("<p><input type=\"password\" name=\"passord\" placeholder=\"Passord\"/></p>");
 		out.println("<p><input type=\"submit\" value=\"Logg inn\"/></p>");
 		out.println("</fieldset></form></body></html>");
 
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		
+
 		String pwin = request.getParameter("passord");
 		String pwEscaped = escapeHtml(pwin);
-				
+
 		String pw = this.getInitParameter("pass");
-		
-		if(!pwEscaped.equals(pw)) {
-			out.println("<html><body>");
-			out.println("<form action=\"loginhandler\" method=\"post\">");
-			out.println("<fieldset>");
-			out.println("<legend><b>Skriv inn passordet ditt</b></legend>");
-			out.println("<p>Passordet du skreiv inn var feil. Prøv på nytt:</p>");
-			out.println("<p><input type=\"password\" name=\"passord\" placeholder=\"Passord\"/></p>");
-			out.println("<p><input type=\"submit\" value=\"Logg inn\"/></p>");
-			out.println("</fieldset></form></body></html>");
+
+		if (!pwEscaped.equals(pw)) {
+			/*
+			 * out.println("<html><body>");
+			 * out.println("<form action=\"loginhandler\" method=\"post\">");
+			 * out.println("<fieldset>");
+			 * out.println("<legend><b>Skriv inn passordet ditt</b></legend>");
+			 * out.println("<p>Passordet du skreiv inn var feil. Prøv på nytt:</p>"); out.
+			 * println("<p><input type=\"password\" name=\"passord\" placeholder=\"Passord\"/></p>"
+			 * ); out.println("<p><input type=\"submit\" value=\"Logg inn\"/></p>");
+			 * out.println("</fieldset></form></body></html>");
+			 */
+			response.sendRedirect("loginhandler?feilPassord");
 		} else {
 			HttpSession sesjon = request.getSession(false);
-			if(sesjon != null) {
+			if (sesjon != null) {
 				sesjon.invalidate();
 			} else {
 				sesjon = request.getSession(true);
 				sesjon.setMaxInactiveInterval(30);
-				sesjon.setAttribute("password", request.getParameter("passord"));
+				sesjon.setAttribute("liste", new HandleVogn());
+				
+				response.sendRedirect("handlelista");
 			}
-			
-			response.sendRedirect("handlelista");
+
+			// response.sendRedirect("handlelista");
 		}
 
-		
 	}
 
 }
